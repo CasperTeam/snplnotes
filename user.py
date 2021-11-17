62faa1cbd8be6e988a7714517c4d60f88b3bda98
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2021-11-17 06:57:02 +0530

    Update user.py

--- user.py
+++ user.py
@@ -11,7 +11,6 @@ else:
 class User(Client):
     def __init__(self):
         super().__init__(
-            Config.TG_USER_SESSION,
             api_hash=Config.API_HASH,
             api_id=Config.APP_ID,
             workers=4
